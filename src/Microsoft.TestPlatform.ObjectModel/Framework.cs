1685bc6c46687a869d9943a7ca7e2665c9444bc3
AF250329 <AF250329@ncr.com>
2022-02-15 22:07:54 +0200

    Set default framework to full .NET Framework in runsettings

--- src/Microsoft.TestPlatform.ObjectModel/Framework.cs
+++ src/Microsoft.TestPlatform.ObjectModel/Framework.cs
@@ -18,15 +18,20 @@ public class Framework
     {
     }
 
+    //    /// <summary>
+    //    /// Default .Net target framework.
+    //    /// </summary>
+    //    public static Framework DefaultFramework { get; }
+    //#if NETFRAMEWORK
+    //        = Framework.FromString(".NETFramework,Version=v4.0");
+    //#elif !NETSTANDARD1_0
+    //        = Framework.FromString(".NETCoreApp,Version=v1.0");
+    //#endif
+
     /// <summary>
     /// Default .Net target framework.
     /// </summary>
-    public static Framework DefaultFramework { get; }
-#if NETFRAMEWORK
-        = Framework.FromString(".NETFramework,Version=v4.0");
-#elif !NETSTANDARD1_0
-        = Framework.FromString(".NETCoreApp,Version=v1.0");
-#endif
+    public static Framework DefaultFramework { get; } = Framework.FromString(".NETFramework,Version=v4.0");
 
     /// <summary>
     /// Gets the FullName of framework.
